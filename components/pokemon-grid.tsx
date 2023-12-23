febc143a3e423c8d2484aed1694aef7893d06081
Ojas13-git <79032848+Ojas13-git@users.noreply.github.com>
2023-12-23 16:38:11 +0530

    filter pokemon by text input

--- components/pokemon-grid.tsx
+++ components/pokemon-grid.tsx
@@ -11,8 +11,18 @@ interface PokemonGridProps{
 
 export function PokemonGrid({ pokemonList }: PokemonGridProps) {
   const [searchText, setSearchText] = useState("");
+  
 
   console.log(pokemonList);
+  
+  const searchFilter = (pokemonList: any) => {
+    return pokemonList.filter(
+        (pokemon:any)=> pokemon.name.toLowerCase().includes(searchText.toLowerCase())
+    )
+  }
+
+  const filteredPokemonList = searchFilter(pokemonList);
+
   return (
     <>
       <div>
@@ -31,7 +41,7 @@ export function PokemonGrid({ pokemonList }: PokemonGridProps) {
         <h3 className="text-3xl pt-12 pb-6 text-center">Pokemon Collection</h3>
       </div>
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-3 lg:text-left">
-            {pokemonList.map((pokemon: any)=>{
+            {filteredPokemonList.map((pokemon: any)=>{
                 return (
                     <PokemonCard name={pokemon.name} key={pokemon.id}/>
                 )
