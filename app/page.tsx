4a5e4ae02850bab49f7b07665b5501779098ceab
Ojas13-git <79032848+Ojas13-git@users.noreply.github.com>
2023-12-23 12:42:24 +0530

    getPokemonList API call to Pokemon API

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,13 @@
 import { PokemonCard } from '@/components/pokemon-card'
 import { PokemonGrid } from '@/components/pokemon-grid'
 import Image from 'next/image'
+import { getPokemonList } from '@/lib/pokemonAPI'
 
-export default function Home() {
+export default async function Home() {
+  const pokemonList = await getPokemonList();
   return (
-    <PokemonGrid/>
+    
+    <PokemonGrid pokemonList={pokemonList}/>
     
     
   )
--- components/pokemon-grid.tsx
+++ components/pokemon-grid.tsx
@@ -1,13 +1,18 @@
-"use-client";
+"use client";
 
 import { useState } from "react";
 import { Input } from "./ui/input";
 import { Label } from "./ui/label";
 import { PokemonCard } from "./pokemon-card";
 
-export function PokemonGrid() {
-  // const [searchText, setSearchText] = useState("");
+interface PokemonGridProps{
+    pokemonList: any
+}
 
+export function PokemonGrid({ pokemonList }: PokemonGridProps) {
+  const [searchText, setSearchText] = useState("");
+
+  console.log(pokemonList);
   return (
     <>
       <div>
@@ -16,11 +21,11 @@ export function PokemonGrid() {
           <Label htmlFor="pokemonName">Pokemon Name</Label>
           <Input
             type="text"
-            // value={searchText}
+            value={searchText}
             autoComplete="off"
             id="pokemonName"
             placeholder="Charizard, Pikachu, etc."
-            // onChange={(e) => setSearchText(e.target.value)}
+            onChange={(e) => setSearchText(e.target.value)}
           />
         </div>
         <h3 className="text-3xl pt-12 pb-6 text-center">Pokemon Collection</h3>
